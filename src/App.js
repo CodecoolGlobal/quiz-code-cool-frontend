5c108a47d89a07eee87225b2b2c0d681d217001f
Eszter Kobor <kobor.eszter@gmail.com>
2020-02-11 09:24:59 +0100

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import QuizStarterForm from "./component/QuizStarterForm";
+import QuestionCard from "./component/QuestionCard";
+import Result from "./component/Result";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <div className='App'>
+        <Route exact path='/' component={QuizStarterForm} />
+        <Route exact path='/quiz' component={QuestionCard} />
+        <Route exact path='/results' component={Result} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
