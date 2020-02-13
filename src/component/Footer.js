ca3a2097eb536c36ef7b8e10be96a7a28cab85cb
Eszter Kobor <kobor.eszter@gmail.com>
2020-02-13 22:08:15 +0100

    Change footer message

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,9 +3,5 @@ import React from "react";
 import { FooterContainer } from "../style/MyStyle";
 
 export default function Footer() {
-  return (
-    <FooterContainer>
-      Copyright &copy; 2020 Reacty Budapest, All rights reserved.
-    </FooterContainer>
-  );
+  return <FooterContainer>Copyright &copy; 2020 Reacty</FooterContainer>;
 }
