0a0992a19ab6dc19804cdfad71e0f54f47967b53
MelathStofi <melath.stofi@gmail.com>
2020-04-08 14:27:11 +0200

    delete works properly

--- src/component/questions/QuestionDetails.js
+++ src/component/questions/QuestionDetails.js
@@ -1,80 +1,82 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext } from 'react';
 
-import { QuestionDetailsContext } from "context/QuestionDetailsContext";
+import { QuestionDetailsContext } from 'context/QuestionDetailsContext';
 
-import Answers from "component/questions/Answers";
-import ValidateButton from "component/questions/ValidateButton";
+import Answers from 'component/questions/Answers';
+import ValidateButton from 'component/questions/ValidateButton';
+import DeleteButton from './DeleteButton';
 
 import {
-  H3,
-  Table,
-  ResultTableData,
-  TableRow,
-  TableHead,
-  ContentContainer,
-  FlexContainer,
-} from "../../style/MyStyle";
+	H3,
+	Table,
+	ResultTableData,
+	TableRow,
+	TableHead,
+	ContentContainer,
+	FlexContainer,
+} from '../../style/MyStyle';
 
 export default function QuestionDetails(props) {
-  const { selectedQuestionState, getQuestion } = useContext(
-    QuestionDetailsContext
-  );
-  const question = selectedQuestionState[0];
+	const { selectedQuestionState, getQuestion } = useContext(
+		QuestionDetailsContext
+	);
+	const question = selectedQuestionState[0];
 
-  const { id } = props.match.params;
+	const { id } = props.match.params;
 
-  useEffect(() => {
-    getQuestion(id);
-  }, [id]);
+	useEffect(() => {
+		getQuestion(id);
+	}, [id]);
 
-  return (
-    <ContentContainer>
-      <H3>Details</H3>
-      <FlexContainer>
-        {question == null ? (
-          <React.Fragment></React.Fragment>
-        ) : (
-          <Table>
-            <thead>
-              <TableRow>
-                <TableHead>Question</TableHead>
-                <TableHead>{question.question}</TableHead>
-              </TableRow>
-            </thead>
-            <tbody>
-            <TableRow>
-                <TableHead>Id</TableHead>
-                <ResultTableData>{question.id}</ResultTableData>
-              </TableRow>
-              <TableRow>
-                <TableHead>Creation date</TableHead>
-                <ResultTableData>{question.creationDate}</ResultTableData>
-              </TableRow>
-              <TableRow>
-                <TableHead>Validation date</TableHead>
-                <ResultTableData>
-                  {question.validationDate === null
-                    ? "Not validated yet"
-                    : question.validationDate}
-                </ResultTableData>
-              </TableRow>
-              <TableRow>
-                <TableHead>Created By</TableHead>
-                <ResultTableData>Zokni kutya</ResultTableData>
-              </TableRow>
-              <TableRow>
-                <TableHead>Category</TableHead>
-                <ResultTableData>{question.category.name}</ResultTableData>
-              </TableRow>
-              <TableRow>
-                <TableHead>Possible answers</TableHead>
-                <Answers question={question} />
-              </TableRow>
-            </tbody>
-          </Table>
-        )}
-      </FlexContainer>
-      <ValidateButton />
-    </ContentContainer>
-  );
+	return (
+		<ContentContainer>
+			<H3>Details</H3>
+			<FlexContainer>
+				{question == null ? (
+					<React.Fragment></React.Fragment>
+				) : (
+					<Table>
+						<thead>
+							<TableRow>
+								<TableHead>Question</TableHead>
+								<TableHead>{question.question}</TableHead>
+							</TableRow>
+						</thead>
+						<tbody>
+							<TableRow>
+								<TableHead>Id</TableHead>
+								<ResultTableData>{question.id}</ResultTableData>
+							</TableRow>
+							<TableRow>
+								<TableHead>Creation date</TableHead>
+								<ResultTableData>{question.creationDate}</ResultTableData>
+							</TableRow>
+							<TableRow>
+								<TableHead>Validation date</TableHead>
+								<ResultTableData>
+									{question.validationDate === null
+										? 'Not validated yet'
+										: question.validationDate}
+								</ResultTableData>
+							</TableRow>
+							<TableRow>
+								<TableHead>Created By</TableHead>
+								<ResultTableData>Zokni kutya</ResultTableData>
+							</TableRow>
+							<TableRow>
+								<TableHead>Category</TableHead>
+								<ResultTableData>{question.category.name}</ResultTableData>
+							</TableRow>
+							<TableRow>
+								<TableHead>Possible answers</TableHead>
+								<Answers question={question} />
+							</TableRow>
+						</tbody>
+					</Table>
+				)}
+			</FlexContainer>
+			<ValidateButton />
+			<DeleteButton />
+		</ContentContainer>
+	);
 }
