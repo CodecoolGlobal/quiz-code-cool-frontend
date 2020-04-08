44d540e6d7bf19077ece59804cb841d27a3d814f
Lorya <mircsi.lodi@gmail.com>
2020-04-08 11:20:44 +0200

    Implement new custom quiz

--- src/component/quizzes/custom/newcustomquiz/NewCustomQuiz.js
+++ src/component/quizzes/custom/newcustomquiz/NewCustomQuiz.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import QuestionList from "component/questions/QuestionList";
+import QuestionFilter from "component/questions/QuestionFilter";
+import NewQuizNameInput from "component/inputs/NewQuizNameInput";
+import { ProgressContext } from "context/ProgressContext";
+import { NewQuizContext } from "context/NewQuizContext";
+
+import { QuestionListContainer, H3, Button } from "style/MyStyle";
 
 export default function NewCustomQuiz() {
-  return <div></div>;
+
+  const submit = useContext(NewQuizContext);
+  const [isReadyToProceed, setIsReadyToProceed] = useContext(ProgressContext);
+
+  return (
+    <div>
+      <NewQuizNameInput />
+      <QuestionListContainer>
+        <H3>Select questions</H3>
+        <QuestionFilter />
+        <QuestionList />
+      </QuestionListContainer>
+      <Button disabled={!isReadyToProceed} onClick={submit}>
+        Start Quiz
+      </Button>
+    </div>
+  );
 }
