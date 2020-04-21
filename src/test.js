9e838f206a5aa26895ea065aee55a8bcf73ac118
Eszter Kobor <kobor.eszter@gmail.com>
2020-04-21 12:26:14 +0200

    Change env

---
+++ src/test.js
@@ -0,0 +1,26 @@
+let components = [CategoryProvider, AuthProvider];
+let main = (
+  <Router>
+    <MenuProvider>
+      <Header />
+    </MenuProvider>
+    <DisableRouteWhenAuthenticated exact path='/sign-in' component={AuthForm} />
+    <DisableRouteWhenAuthenticated exact path='/sign-up' component={AuthForm} />
+    <PrivateRoute exact path='/user' component={UserDetails} />
+  </Router>
+);
+
+return components.reduce((prev, curr) => curr(prev), main);
+
+// const arr = [1, 2, 3];
+// console.log(arr.reduce((prevs, curr) => prevs + curr, 0));
+
+{
+  /* <div>
+    <span>
+        <h1>
+            Hello
+        </h1>
+    </span>
+</div> */
+}
