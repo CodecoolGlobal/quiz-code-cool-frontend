a60cf2e6ca7b3d02db4f7db990b556fc9ea785cd
Eszter Kobor <kobor.eszter@gmail.com>
2020-06-05 14:10:55 +0200

    Center footer copyright

--- src/component/layout/footer/Footer.js
+++ src/component/layout/footer/Footer.js
@@ -1,18 +1,21 @@
 import React from "react";
 
-import {
-  FooterContainer,
-  FooterLink,
-  FooterLine
-} from "component/layout/footer/styles";
+import {FooterContainer, FooterItem, FooterItemRight, FooterItemCenter } from "component/layout/footer/styles";
 
 export default function Footer() {
   return (
     <FooterContainer>
-      <FooterLine>Copyright &copy; 2020 Codecool quiz</FooterLine>
-      <FooterLink href='https://reacty.netlify.com/' target='_blank'>
-        Reacty
-      </FooterLink>
+      <FooterItem></FooterItem>
+      <FooterItem>
+        <FooterItemRight>
+          <a href='https://reacty.netlify.com/' target='_blank'>
+            Reacty
+          </a>
+        </FooterItemRight>
+      </FooterItem>
+      <FooterItem>
+        <FooterItemCenter>Copyright &copy; 2020 Codecool quiz</FooterItemCenter>
+      </FooterItem>
     </FooterContainer>
   );
 }
--- src/component/layout/footer/styles.js
+++ src/component/layout/footer/styles.js
@@ -2,29 +2,36 @@ import styled from "styled-components";
 import { applicationTheme } from "style/js/CommonStyles";
 
 export const FooterContainer = styled.div`
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-font-size: ${applicationTheme.fontSize1};
-color: ${applicationTheme.color1};
-background: #fff;
-text-align: center;
+  padding: 1%;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 98%;
+  font-size: ${applicationTheme.fontSize1};
+  color: ${applicationTheme.color1};
+  background: #fff;
 `;
 
-export const FooterLine = styled.div`
-font-family: "Capriola", sans-serif;
-float: left;
-padding: 10px 15px;
+export const FooterItem = styled.div`
+  float: right;
+  width: 33%;
 `;
 
-export const FooterLink = styled.a`
-float: right;
-padding: 10px 15px;
-text-decoration: none;
-color: ${applicationTheme.color1};
-font-size: ${applicationTheme.fontSize1};
-&:hover {
-  text-decoration: underline;
-}
+export const FooterItemCenter = styled.div`
+  font-family: "Capriola", sans-serif;
+  font-weight: bold;
+`;
+
+export const FooterItemRight = styled.div`
+  text-align: right;
+  & a {
+    text-decoration: none;
+    color: ${applicationTheme.color1};
+    font-size: ${applicationTheme.fontSize1};
+    &:hover {
+      text-decoration: underline;
+      font-weight: bold;
+      color: ${applicationTheme.orange}
+    }
+  }
 `;
