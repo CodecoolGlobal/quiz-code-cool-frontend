cd644cd5df27f6766c88f32b80df4ed8ba96f57c
Eszter Kobor <kobor.eszter@gmail.com>
2020-06-09 22:42:18 +0200

    Delete 403 from handleError

--- src/util/errorUtil.js
+++ src/util/errorUtil.js
@@ -2,10 +2,6 @@ export const handleError = (error) => {
 
   let message = `Something went wrong! :(` 
   if (error.response) {
-      if (error.response.status === 403) {
-
-        return;
-      }
       const errorResponseData = error.response.data
       message += `\nMessage: ${errorResponseData.message}\nTime: ${errorResponseData.timestamp}`
   } else {
